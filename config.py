a46a87a06792286f3cacfea28caa5cf1bd0953b9
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-08-08 19:22:59 +0300

    Add enviroment variables.

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
 USE_ROUNDED_COORDS = True
-OPEN_WEATHER_API = '996fc0e3726aece7cf7f959bd6333d56'
+OPEN_WEATHER_API = env.str('OPEN_WEATHER_API')
 OPEN_WEATHER_URL = (
         'https://api.openweathermap.org/data/2.5/weather?'
         'lat={latitude}&lon={longitude}&'
         'appid=' + OPEN_WEATHER_API + '&lang=ru&'
         'units=metric'
         )
+
+
+if __name__ == '__main__':
+    print(OPEN_WEATHER_API)
 
